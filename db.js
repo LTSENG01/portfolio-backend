de18ebbc691770d5de38f8f6f8804f6ad1e11143
Larry Tseng <larry.tseng.1@gmail.com>
2020-05-29 01:25:17 -0400

    Add function stubs and code for routing

--- db.js
+++ db.js
@@ -6,10 +6,15 @@ const pool = mysql.createPool({
     database: process.env.LT_DB_DATABASE
 });
 
-let runQuery = (query) => {
+/**
+ *
+ * @param query
+ * @return {Promise<RowDataPacket[]>}
+ */
+let runQuery = query => {
     return new Promise((resolve, reject) => {
         pool.query(query, (err, rows) => {
-            if (err) {
+            if (err || rows.length === 0) {
                 reject(err);
             }
             resolve(rows);
@@ -17,10 +22,39 @@ let runQuery = (query) => {
     });
 };
 
+let logQuery = () => {
+
+};
+
+let routeResolver = (path, callback) => {
+    const query = "SELECT route FROM routes WHERE path = " + path;
+    runQuery(query)
+        .then(rowCheckerUtil)
+        .then()     // update log   todo
+        .then(rows => callback(rows[0].route))
+        .catch(callback(404));
+};
+
+let referrerResolver = (refID, callback) => {
+    const query = "SELECT route_id FROM referrers WHERE ref_string = " + refID;     // Todo: NOT VALID
+    runQuery(query)
+        .then(rowCheckerUtil)
+        .then(rows => callback(rows[0].route))
+        .catch(callback(404));
+};
+
+// todo test
+let rowCheckerUtil = rows => {
+    if (rows.length > 1 || rows[0].route === undefined) {
+        throw new Error("Invalid number of rows for such path");
+    }
+}
+
 runQuery("SELECT route FROM routes WHERE path = 'umass'")
-    .then(console.log)
+    .then(r => console.log(r[0].route))
     .catch(console.error);
 
 module.exports = {
-    runQuery: runQuery
+    routeResolver: routeResolver,
+    referrerResolver: referrerResolver
 }
