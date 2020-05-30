7252d8bfd9239873e7555b5f2b0c0c293ba56bf9
Larry Tseng <larry.tseng.1@gmail.com>
2020-05-30 00:35:16 -0400

    Main db methods are almost finished

--- db.js
+++ db.js
@@ -1,10 +1,15 @@
 const mysql = require('mysql');
+const axios = require('axios').default;
+
 const pool = mysql.createPool({
     host: process.env.LT_DB_HOST,
     user: process.env.LT_DB_USER,
     password: process.env.LT_DB_PASSWORD,
     database: process.env.LT_DB_DATABASE
 });
+
+const ipstackUrl = "http://api.ipstack.com/";
+const ipstackKey = process.env.IPSTACK_API;
 
 /**
  *
@@ -31,27 +36,49 @@ let runQuery = async query => {
  * @return {Promise<*>}
  */
 let logQuery = async (route, referrer, req) => {
-    const query = "INSERT INTO log (route_id, referrer_id, request, ip, location, browser, referer) VALUES ("
+    try {
+        let location;
+
+        try {
+            if (req.ip !== '::1') {
+                location = (await axios.get(ipstackUrl + req.ip + "?access_key=" + ipstackKey)).data;
+                location = location["city"].concat(", ", location["region_name"], ", ", location["country_code"]);
+            }
+        } catch (err) {
+            console.error(err);
+        }
+
+        const query = "INSERT INTO log (route_id, referrer_id, request, ip, location, useragent, referer) " +
+                        `VALUES ((SELECT id FROM routes WHERE route = '${route}'), ` +
+                        `(SELECT id FROM referrers WHERE ref_string = '${referrer}'), ` +
+                        `'${req.originalUrl}', '${req.ip}', '${location}', ` +
+                        `'${req.get('User-Agent').substring(0, 511)}', '${req.get('Referer')}')`;
+
+        await runQuery(query)
+    } catch (err) {
+        console.error(err);
+    }
+
     return route;
 };
 
 let routeResolver = (path, req, callback) => {
-    const query = "SELECT route FROM routes WHERE path = '" + path + "'";
+    const query = `SELECT route FROM routes WHERE path = '${path}'`;
     runQuery(query)
         .then(routeCheckerExtractor)
         .then(route => logQuery(route, undefined, req))
         .then(callback)
-        .catch(() => callback(404));
+        .catch(err => callback(404, err));
 };
 
 let referrerResolver = (refID, req, callback) => {
     const query = "SELECT routes.route FROM referrers INNER JOIN routes " +
-        "WHERE routes.id = referrers.route_id AND referrers.ref_string = '" + refID + "'";
+        `WHERE routes.id = referrers.route_id AND referrers.ref_string = '${refID}'`;
     runQuery(query)
         .then(routeCheckerExtractor)
         .then(route => logQuery(route, refID, req))
         .then(callback)
-        .catch(() => callback(404));
+        .catch(err => callback(404, err));
 };
 
 /**
@@ -66,12 +93,8 @@ let routeCheckerExtractor = rows => {
     return rows[0].route;
 }
 
-// runQuery("SELECT route FROM routes WHERE path = 'umass'")
-//     .then(r => console.log(r[0].route))
-//     .catch(console.error);
-
-routeResolver('umass', undefined, console.log);
-referrerResolver('8f760259f7a44c2d', undefined, console.log)
+// routeResolver('umass', undefined, console.log);
+// referrerResolver('8f760259f7a44c2d', undefined, console.log)
 
 module.exports = {
     routeResolver: routeResolver,
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const db = require('../db.js');
+
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/*', function(req, res, next) {
+  db.routeResolver(req.path, req, (route, err=false) => {
+    if (err) {
+      console.log(err);
+      // res.status(404);
+    } else {
+      console.log("Route: " + route);
+    }
+    res.redirect(302, route);
+  })
 });
 
 module.exports = router;
